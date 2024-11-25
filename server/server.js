abfa20e823d03c6169a330e476a24f89fbc15b1d
Divya9684 <divyabhavar4@gmail.com>
2024-11-25 15:32:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ mongoose
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"], // Allow both origins
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -41,7 +41,7 @@ app.use(
       "Expires",
       "Pragma",
     ],
-    credentials: true,
+    credentials: true, // Allow cookies and credentials
   })
 );
 app.use(cookieParser());
